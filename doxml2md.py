eead16a02b39bcefb45bebc3fd2a11a13796d070
filipecn <fuiripecn@gmail.com>
2017-08-19 13:40:16 -0300

    first

---
+++ doxml2md.py
@@ -0,0 +1,174 @@
+#!/usr/bin/python
+
+import sys
+from subprocess import call
+from subprocess import check_output
+import xml.etree.ElementTree as ET
+from jinja2 import Template
+import re
+import importlib
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+def getList(node, child, field, value):
+    return list(filter(lambda c: c.get(field) == value, node.findall(child)))
+
+
+def getInstanceList(className, node, child):
+    class_ = getattr(importlib.import_module("doxml2md"), className)
+    instList = []
+    if node is not None:
+        instList = [class_(t) for t in node.findall(child)]
+    instList = sorted(instList, key=lambda x: x.name)
+    return instList
+
+
+def getOne(node, child, field, value):
+    instList = list(
+        filter(lambda c: c.get(field) == value, node.findall(child)))
+    if len(instList) > 0:
+        return instList[0]
+    return None
+
+
+class Compound:
+    def __init__(self, c):
+        self.object = c
+        try:
+            self.name = c.find('compoundname').text
+        except:
+            self.name = ""
+        self.kind = c.get('kind')
+        self.id = c.get('id')
+        self.briefdescription = ""
+        self.detaileddescription = ""
+        if c.find('briefdescription').find('para') is not None:
+            self.briefdescription = c.find('briefdescription').find(
+                'para').text
+        if c.find('detaileddescription').find('para') is not None:
+            self.detaileddescription = \
+                c.find('detaileddescription').find('para').text
+
+
+class Param:
+    def __init__(self, m, p):
+        self.type = p.find('type').text
+        try:
+            self.name = p.find('declname').text
+        except:
+            self.name = ""
+        self.detaileddescription = ""
+        try:
+            paramList = getOne(
+                m.find('detaileddescription').find('para'), 'parameterlist',
+                'kind', 'param')
+            if paramList is not None:
+                pList = paramList.findall('parameteritem')
+                for pl in pList:
+                    if pl.find('parameternamelist')\
+                            .find('parametername').text == self.name:
+                        self.detaileddescription = \
+                            pl.find('parameterdescription').find('para').text
+        except:
+            pass
+
+
+class Method(Compound):
+    def __init__(self, m):
+        Compound.__init__(self, m)
+        self.name = m.find('name').text
+        self.object = m
+        self.definition = m.find('definition').text
+        self.args = m.find('argsstring').text
+        self.type = m.find('type').text
+        self.params = [Param(m, p) for p in m.findall('param')]
+        self.returnDescription = ''
+        try:
+            if self.type is not None and self.type != 'void'\
+                    and self.type != '':
+                print self.definition
+                self.returnDescription = m.find('detaileddescription') \
+                    .find('para').find('simplesect').find('para').text
+        except:
+            pass
+
+
+class Class(Compound):
+    def __init__(self, cl):
+        Compound.__init__(self, cl)
+        print self.name
+        try:
+            self.includes = cl.find('includes').text
+        except:
+            self.includes = ""
+        self.fields = getList(cl, 'sectiondef', 'kind', 'public-attribute')
+        methodList = getOne(cl, 'sectiondef', 'kind', 'public-func')
+        self.methods = getInstanceList('Method', methodList, 'memberdef')
+
+
+class Group(Compound):
+    def __init__(self, g):
+        Compound.__init__(self, g)
+
+
+class Namespace(Compound):
+    def __init__(self, n, classList):
+        Compound.__init__(self, n)
+        self.classes = [
+            list(filter(lambda c: c.id == cl.get('refid'), classList))[0]
+            for cl in n.findall('innerclass')
+        ]
+        self.classes = sorted(self.classes, key=lambda x: x.name)
+        functionList = getOne(n, 'sectiondef', 'kind', 'func')
+        self.functions = getInstanceList('Method', functionList, 'memberdef')
+
+
+class Typedef(Compound):
+    def __init__(self, t):
+        Compound.__init__(self, t)
+        self.name = t.find('name').text
+        self.definition = t.find('definition').text
+
+
+class File(Compound):
+    def __init__(self, f):
+        Compound.__init__(self, f)
+        typedefList = getOne(f, 'sectiondef', 'kind', 'typedef')
+        self.typedefs = getInstanceList('Typedef', typedefList, 'memberdef')
+
+
+def cleanName(n):
+    return n.replace("::", "").replace("<", "").replace(">", "").replace(
+        ",", "").replace(" ", "")
+
+
+def doxml2py(xml_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    groups = [
+        Group(gp)
+        for gp in list(
+            filter(lambda child: child.get('kind') == 'group', root))
+    ]
+    groups = sorted(groups, key=lambda x: x.name)
+    classes = [
+        Class(cl)
+        for cl in list(
+            filter(
+                lambda child: child.get('kind') == 'class' or child.get('kind') == 'struct' or child.get('kind') == 'union',
+                root))
+    ]
+    classes = sorted(classes, key=lambda x: x.name)
+    files = [
+        File(f)
+        for f in list(filter(lambda child: child.get('kind') == 'file', root))
+    ]
+    files = sorted(files, key=lambda x: x.name)
+    namespaces = [
+        Namespace(n, classes)
+        for n in list(
+            filter(lambda child: child.get('kind') == 'namespace', root))
+    ]
+    namespaces = sorted(namespaces, key=lambda x: x.name)
